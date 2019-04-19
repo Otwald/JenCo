904c7dbe907897de7ea74c5aeb3830633fe3ba5f
otwald <hinterlanddev@googlemail.com>
2019-04-19 07:33:00 +0200

    working on sidebar

--- imports/ui/Sidebar.js
+++ imports/ui/Sidebar.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import Welcome from './Welcome';
 export default class Sidebar extends React.Component{
     render(){
         return(
             <div className="Sidebar">
-                Sidebar
+                <ul>
+                    <li>Sidebar</li>
+                    <li><Welcome/></li>
+                </ul>
+                
             </div>
             
         )
