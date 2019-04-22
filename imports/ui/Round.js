1df6771db91cc7dd5c4828dbcad783d9a7461d3a
otwald <hinterlanddev@googlemail.com>
2019-04-22 10:26:08 +0200

    inserted timeblocks for rounds

--- imports/ui/Round.js
+++ imports/ui/Round.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import { Dropdown } from 'semantic-ui-react';
 
 import { Rounds } from '../api/mongo_export';
 
 
-const time_block = ['early', 'later']
+const time_block = [{ text: 'Früh', value: 'early' }, { text: 'Spät', value: 'later' }]
 export default class Round extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             round_create: {
-                round_tb : '',
+                round_tb: '',
                 round_name: '',
                 setting: '',
                 ruleset: '',
@@ -27,6 +28,12 @@ export default class Round extends React.Component {
         var temp = this.state.round_create
         var value = e.target.value
         temp[e.target.name] = value
+        this.setState({ round_create: temp })
+    }
+
+    onInputBlock = (e, data) => {
+        var temp = this.state.round_create
+        temp[data.type] = data.value
         this.setState({ round_create: temp })
     }
 
@@ -68,11 +75,11 @@ export default class Round extends React.Component {
                 return k
             }
         })
-        if(index.length !== 0){
-            index.map((k,v)=>{
-                if(k){
+        if (index.length !== 0) {
+            index.map((k, v) => {
+                if (k) {
                     data.round_curr_pl--
-                    data.round_player.splice(k,1);
+                    data.round_player.splice(k, 1);
                 }
             })
         }
@@ -98,6 +105,41 @@ export default class Round extends React.Component {
             }))
         }
         return out
+    }
+
+    timeBlockCreate = (time) => {
+        const rounds_box = this.props.rounds_box
+        var round = ''
+        if (rounds_box.length !== 0) {
+            var out = '';
+            round = rounds_box.map((k, v) => {
+                if (k.round_tb === time) {
+                    if (Meteor.userId()) {
+                        if (k.round_gm_id === this.props.loginToken) {
+                            out = <li><button onClick={() => this.onEdit(k)} >Edit</button></li>
+                        } else if (this.onCheck(k.round_player)) {
+                            out = <li><button onClick={() => this.onJoin(k)} >Join</button></li>
+                        } else {
+                            out = <li><button onClick={() => this.onLeave(k)} >Leave</button></li>
+                        }
+                    }
+                    return (
+                        <div key={v}>
+                            <ul>
+                                <li>Name = {k.round_name}</li>
+                                <li>Setting = {k.setting}</li>
+                                <li>Regelwerk = {k.ruleset}</li>
+                                <li>Spielleiter = {k.round_gm}</li>
+                                <li>Spieler Zahl/Max = {k.round_curr_pl}/{k.round_max_pl}</li>
+                                <li>Spieler Namen = {this.onPlayers(k.round_player)}</li>
+                                {out}
+                            </ul>
+                        </div>
+                    )
+                }
+            })
+        }
+        return round
     }
 
     render() {
@@ -134,6 +176,15 @@ export default class Round extends React.Component {
         if (loginToken) {
             create = <div>
                 <ul>
+                    <li>
+                        ZeitBlock<Dropdown
+                            placeholder='Block'
+                            options={time_block}
+                            scrolling
+                            onChange={this.onInputBlock}
+                            type='round_tb'
+                        />
+                    </li>
                     <li>Runden Name<input type='text' name='round_name' onChange={this.onInput} placeholder={this.state.round_create.round_name} /></li>
                     <li>Setting<input type='text' name='setting' onChange={this.onInput} placeholder={this.state.round_create.setting} /></li>
                     <li>Regelwerk <input type='text' name='ruleset' onChange={this.onInput} placeholder={this.state.round_create.ruleset} /></li>
@@ -145,9 +196,9 @@ export default class Round extends React.Component {
         return (
             <div>
                 Early Block
-                {round}
+                {this.timeBlockCreate('early')}
                 Late Block
-                {round}
+                {this.timeBlockCreate('later')}
                 Hidden Block
                 {create}
             </div >
