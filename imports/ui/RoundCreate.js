78938da12a55596dbe3cce3162344fad926af46e
otwald <hinterlanddev@googlemail.com>
2019-06-15 12:00:07 +0200

    RoundCreate Design added

--- imports/ui/RoundCreate.js
+++ imports/ui/RoundCreate.js
@@ -8,25 +8,26 @@ const roundCreate = props => {
     const [tableOptions, setTableOptions] = useState([]);
     useEffect(() => {
         setTableOptions(props.tableOptions);
-        return (()=>{
+        return (() => {
             setTableOptions([]);
         })
     }, [props.time_block, props.round_create, props.tableOptions])
     let create = '';
     if (Meteor.userId()) {
-        create = <div>
-            <ul>
-                <li>
-                    ZeitBlock<Dropdown
+        create = <form>
+            <div className='form-row justify-content-center'>
+                <div className='form-group col-sm-2'>
+                    <label>ZeitBlock</label>
+                    <Dropdown
                         placeholder='Block'
                         options={props.time_block}
                         scrolling
                         onChange={props.onInputBlock}
                         type='round_tb'
                     />
-                </li>
-                <li>
-                    Tisch Nummer
+                </div>
+                <div className='form-group col-sm-3'>
+                    <label >Tisch Nummer</label>
                     <Dropdown
                         placeholder='Block'
                         options={props.tableOptions}
@@ -34,14 +35,42 @@ const roundCreate = props => {
                         onChange={props.onInputBlock}
                         type='round_table'
                     />
-                </li>
-                <li>Runden Name<input type='text' name='round_name' onChange={props.onInput} placeholder={props.round_create.round_name} /></li>
-                <li>Setting<input type='text' name='setting' onChange={props.onInput} placeholder={props.round_create.setting} /></li>
-                <li>Regelwerk <input type='text' name='ruleset' onChange={props.onInput} placeholder={props.round_create.ruleset} /></li>
-                <li>Spieler Max <input type='text' name='round_max_pl' onChange={props.onInput} placeholder={props.round_create.round_max_pl} /></li>
-                <li><button onClick={props.onSave} >Save</button></li>
-            </ul>
-        </div>
+                </div>
+            </div>
+            <div className='form-group row justify-content-center'>
+                <label className="col-sm-2 col-form-label">
+                    Runden Name
+                    </label>
+                <div className='col-sm-10'>
+                    <input type='text' name='round_name' className='form-control' onChange={props.onInput} placeholder={props.round_create.round_name} />
+                </div>
+            </div>
+            <div className='form-group row justify-content-center'>
+                <label className="col-sm-2 col-form-label">
+                    Setting
+                </label>
+                <div className='col-sm-10'>
+                    <input type='text' name='setting' className='form-control' onChange={props.onInput} placeholder={props.round_create.setting} />
+                </div>
+            </div>
+            <div className='form-group row justify-content-center'>
+                <label className="col-sm-2 col-form-label">
+                    Regelwerk
+                    </label>
+                <div className='col-sm-10'>
+                    <input type='text' name='ruleset' className='form-control' onChange={props.onInput} placeholder={props.round_create.ruleset} />
+                </div>
+            </div>
+            <div className='form-group row justify-content-center'>
+                <label className="col-sm-2 col-form-label">
+                    Spieler Max
+                </label>
+                <div className='col-sm-10'>
+                    <input type='text' name='round_max_pl' className='form-control' onChange={props.onInput} placeholder={props.round_create.round_max_pl} />
+                </div>
+            </div>
+            <button className='btn btn-outline-dark' onClick={props.onCancel} >Abbruch</button><button className='btn btn-outline-dark' onClick={props.onSave} >Speichern</button>
+        </form>
     }
     return (
         <div>
