20108748de68852da7c80efce95a5687e002b333
otwald <hinterlanddev@googlemail.com>
2019-07-06 10:12:01 +0200

    added fullset of needed params to RoundCreate

--- imports/ui/RoundCreate.js
+++ imports/ui/RoundCreate.js
@@ -5,13 +5,6 @@ import { Dropdown } from 'semantic-ui-react';
 
 const roundCreate = props => {
 
-    const [tableOptions, setTableOptions] = useState([]);
-    useEffect(() => {
-        setTableOptions(props.tableOptions);
-        return (() => {
-            setTableOptions([]);
-        })
-    }, [props.time_block, props.round_create, props.tableOptions])
     let create = '';
     if (Meteor.userId()) {
         create = <form>
@@ -38,38 +31,72 @@ const roundCreate = props => {
                 </div>
             </div>
             <div className='form-group row justify-content-center'>
-                <label className="col-sm-2 col-form-label">
+                <label className="col-sm-3 col-form-label">
                     Runden Name
                     </label>
-                <div className='col-sm-10'>
+                <div className='col-sm-9'>
                     <input type='text' name='round_name' className='form-control' onChange={props.onInput} placeholder={props.round_create.round_name} />
                 </div>
             </div>
             <div className='form-group row justify-content-center'>
-                <label className="col-sm-2 col-form-label">
+                <label className="col-sm-3 col-form-label">
                     Setting
                 </label>
-                <div className='col-sm-10'>
+                <div className='col-sm-9'>
                     <input type='text' name='setting' className='form-control' onChange={props.onInput} placeholder={props.round_create.setting} />
                 </div>
             </div>
             <div className='form-group row justify-content-center'>
-                <label className="col-sm-2 col-form-label">
+                <label className="col-sm-3 col-form-label">
                     Regelwerk
                     </label>
-                <div className='col-sm-10'>
+                <div className='col-sm-9'>
                     <input type='text' name='ruleset' className='form-control' onChange={props.onInput} placeholder={props.round_create.ruleset} />
                 </div>
             </div>
             <div className='form-group row justify-content-center'>
-                <label className="col-sm-2 col-form-label">
-                    Spieler Max
+                <label className="col-sm-3 col-form-label">
+                    Spielerplätze Maximal
                 </label>
-                <div className='col-sm-10'>
-                    <input type='text' name='round_max_pl' className='form-control' onChange={props.onInput} placeholder={props.round_create.round_max_pl} />
+                <div className='col-sm-9'>
+                    <input type='number' name='round_max_pl' className='form-control' onChange={props.onInput} placeholder={props.round_create.round_max_pl} />
                 </div>
             </div>
-            <button className='btn btn-outline-dark' onClick={props.onCancel} >Abbruch</button><button className='btn btn-outline-dark' onClick={props.onSave} >Speichern</button>
+            <div className='form-group row justify-content-center'>
+                <label className="col-sm-3 col-form-label">
+                    Maximal Online Anmeldungsplätze
+                </label>
+                <div className='col-sm-9'>
+                    <input type='number' name='round_max_online_pl' className='form-control' onChange={props.onInput} placeholder={props.round_create.round_max_online_pl} />
+                </div>
+            </div>
+            <div className='form-group row justify-content-center'>
+                <label className="col-sm-3 col-form-label">
+                    Vorgefertigte Charakter
+                </label>
+                <div className='col-sm-9'>
+                    <div className='form-check form-check-inline'>
+                        <input type='radio' name='own_char' id='inlineradiochar1' className='form-check-input' onChange={props.onInput} value='true' />
+                        <label className="form-check-label">Ja</label>
+
+                    </div>
+                    <div className='form-check form-check-inline'>
+                        <input type='radio' name='own_char' id='inlineradiochar2' className='form-check-input' onChange={props.onInput} value='false' />
+                        <label className="form-check-label">Nein</label>
+                    </div>
+                </div>
+            </div>
+            <div className='form-group row justify-content-center'>
+                <label className="col-sm-3 col-form-label">
+                    Rundenbeschreibung
+                </label>
+                <div className='col-sm-9'>
+                    <textarea type='number' name='round_desc' className='form-control' onChange={props.onInput} placeholder={props.round_create.round_desc} />
+                </div>
+            </div>
+            <div className='form-group row justify-content-center'>
+                <button className='btn btn-outline-dark col-sm-4' onClick={props.onCancel} >Abbruch</button><button className='btn btn-outline-dark col-sm-4' onClick={props.onSave} >Speichern</button>
+            </div>
         </form>
     }
     return (
