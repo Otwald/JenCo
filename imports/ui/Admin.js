beaea5e4ee4f007cdc8c39f67d324569d57e1bfc
otwald <hinterlanddev@googlemail.com>
2019-07-14 09:20:37 +0200

    rework design usermanagment

--- imports/ui/Admin.js
+++ imports/ui/Admin.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'semantic-ui-react';
 
 import AdminBlock from './AdminBlock';
 
@@ -21,6 +20,9 @@ const admin = props => {
     const [tbTab, setTbTab] = useState(false);
     const [eventEdit, setEventEdit] = useState(false);
     const [eventPay, setEventPay] = useState(0);
+    const [agefilter, setAgefilter] = useState(false);
+    const [payfilter, setPayfilter] = useState(false);
+    const [deleteUser, setDeleteUser] = useState('');
 
     useEffect(() => {
         if (props.event) {
@@ -94,14 +96,35 @@ const admin = props => {
         );
     }
 
-    //for user table
-    mouseIn = (e) => {
-        if (activeUser !== e.target.value) {
-            setActiveUser(e.target.value);
+    /**
+     * handles the click event on the user managment table,
+     * and saves user id in a state
+     * @param Object data holds db Entry to user
+     */
+    mouseIn = (data) => {
+        if (activeUser !== data._id) {
+            setActiveUser(data._id);
         } else {
             setActiveUser(null)
         }
+    }
 
+    /**
+     * small helper to convert timestamp age into
+     * age in years with the event as the measurement 
+     * 
+     * @param Number timestamp holds the age in Timestamp format
+     * @return Number Age 
+     */
+    calcAge = (timestamp) => {
+        if (props.event) {
+            event_start = new Date(props.event.e_start)
+            age = event_start.getTime() - timestamp;
+            age = new Date(age);
+            age = age.getFullYear() - 1970;
+            return age;
+        }
+        return 0;
     }
 
     onTabChange = (e) => {
@@ -111,46 +134,112 @@ const admin = props => {
     }
 
     let user_block = '';
+    let userMan = '';
     if (props.users.length > 0) {
         props.users.sort((a, b) => (a.last > b.last) ? 1 : ((b.last > a.last) ? -1 : 0));
         user_block = props.users.map((key, value) => {
-            // Namens Sortierung | Radial alle , bezahlt, nicht bezahlt | Minderjährige
+            let age = calcAge(key.age)
+            if (agefilter === true) {
+                if (age > 16) {
+                    return;
+                }
+            }
+            if (payfilter === true) {
+                if (key.bill === true) {
+                    return;
+                }
+            }
             return (
-                <tr onClick={(e) => this.mouseIn(e)} key={'User' + value} value={value}>
+                <tr onClick={() => {
+                    this.mouseIn(key)
+                    setDeleteUser('')
+                }} key={'Table' + value}>
+                    <th scope='row'>{value + 1}</th>
                     <th>{key.last}</th>
                     <th>{this.outPay(key.bill)}</th>
-                    {value === activeUser ?
-                        <ul>
-                            <li>{key.first}</li>
-                            <li>{key.last}</li>
-                            <li>{key.profil}</li>
-                            <li>{key.age}</li>
-                            <li>{key.email}</li>
-                            <li><Button onClick={(e) => Meteor.call('SwitchBill', key._id)} >Switch Pay</Button></li>
-                            <li><Button onClick={(e) => this.onClickUserConfirm(key)} >Bestätigung</Button><Button onClick={(e) => this.onClickUserDestroy(key)} >Destroy</Button></li>
-                        </ul>
-                        : ""}
+                    <th>{age}</th>
                 </tr>
+            )
+        })
+        userMan = props.users.map((value, index) => {
+            return (
+
+                <React.Fragment key={'User' + index}>
+                    {value._id == activeUser ?
+                        <div className="card col-sm-8">
+                            <div className='card-body'>
+                                <div className='row'>
+                                    <label className='col-sm-4'><strong>Profil Name</strong></label>
+                                    <div className='col-sm-8 text-muted'>{value.profil}</div>
+                                </div>
+                                <div className='row'>
+                                    <label className='col-sm-4'><strong>Vorname</strong></label>
+                                    <div className='col-sm-8 text-muted'>{value.first}</div>
+                                </div>
+                                <div className='row'>
+                                    <label className='col-sm-4'><strong>Nachname</strong></label>
+                                    <div className='col-sm-8 text-muted'>{value.last}</div>
+                                </div>
+                                <div className='row'>
+                                    <label className='col-sm-4'><strong>Alter</strong></label>
+                                    <div className='col-sm-8 text-muted'>{new Date(value.age).toDateString()}</div>
+                                </div>
+                                <div className='row'>
+                                    <label className='col-sm-4'><strong>Status</strong></label>
+                                    <div className='col-sm-8 text-muted'>{value.bill ? 'Ja' : 'Nein'}</div>
+                                </div>
+                                <div className='row'>
+                                    <label className='col-sm-4'><strong>E-Mail</strong></label>
+                                    <div className='col-sm-8 text-muted'>{value.email}</div>
+                                </div>
+                                <div className='row justify-content-center'>
+                                    <button className='btn btn-outline-dark col-sm-4' onClick={(e) => Meteor.call('SwitchBill', value._id)} >Wechsel Bezahlstatus</button>
+                                    <button className='btn btn-outline-dark col-sm-4' onClick={(e) => this.onClickUserConfirm(value)} >BestätigungsEmail</button>
+                                </div>
+                                <div className='row justify-content-center'>
+                                    <button className='btn btn-outline-dark col-sm-4' onClick={() => setDeleteUser(value._id)} >Nutzer löschen</button>
+                                    {deleteUser == value._id ?
+                                        <React.Fragment>
+                                            <button className='btn btn-outline-dark col-sm-4' onClick={() => setDeleteUser('')}>Abbruch</button>
+                                            <button className='btn btn-outline-dark col-sm-4' onClick={(e) => this.onClickUserDestroy(value)}>Wirklich Löschen?</button>
+                                        </React.Fragment>
+                                        :
+                                        ''}
+                                </div>
+                            </div>
+                        </div>
+                        : ""}
+                </React.Fragment>
             )
         })
     }
     return (
         <React.Fragment >
             <div className="row">
-                <div className='col-sm-3' onClick={() => setUserTab(!userTab)}>Nutzerverwaltung</div>
+                <div className='col-sm-3' onClick={() => {
+                    setUserTab(!userTab)
+                    setDeleteUser('')
+                }}>Nutzerverwaltung</div>
                 {userTab ?
                     <div className='col-sm-9'>
-                        <table className='table'>
-                            <thead>
-                                <tr>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {user_block}
-                            </tbody>
-                        </table>
+                        <div className='table-responsive-sm'>
+                            <table className='table table-striped table-hover'>
+                                <thead>
+                                    <tr>
+                                        <th scope='col'>#</th>
+                                        <th scope="col">Nachname</th>
+                                        <th scope="col" onClick={() => setPayfilter(!payfilter)}>Status<p className='text-muted'>{payfilter ? 'nicht bezahlt' : 'Alle'}</p></th>
+                                        <th scope='col' onClick={() => setAgefilter(!agefilter)}>Alter<p className='text-muted'>{agefilter ? 'Jung' : 'Alle'}</p></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {user_block}
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className='row justify-content-center'>
+                            {userMan}
+                        </div>
                     </div>
                     : ''}
             </div>
