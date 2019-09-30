a776d330547f218d15bfdcae150b51c814d120c2
otwald <hinterlanddev@googlemail.com>
2019-09-30 22:25:21 +0200

    updatet front page still static

--- imports/ui/Welcome.js
+++ imports/ui/Welcome.js
@@ -16,34 +16,35 @@ const welcome = props => {
         <div className='row'>
             <div className='col-sm'>
                 <p className="text-sm-left text-break">
-                    Hallo zusammen,<br />
-                    wenn du dir diese Seite ansiehst, wurdest du vermutlich zu unserer kleinen
-                    Pen&Paper-Con im Oktober diesen Jahres eingeladen. Hier noch einmal die Fakten:<br />
+                    Hallo zusammen und willkommen auf unserer schönen Website!<br />
                     <br />
-                    <b>Wann?</b> 12. bis 13. Oktober 2019, ab 12 Uhr am Samstag bis 14 Uhr am Sonntag<br />
+                    Diese vorläufige Version dient nur dazu, euch einen Überblick über die angemeldeten Runden zu geben – ignoriert den Anmelde-Button, der tut nichts.
+                    Ansonsten: hier noch einmal alle Eckdaten unserer Veranstaltung.
                     <br />
-                    <b>Wo?</b>{loco}<br />
+                    <b>Wann?</b> 12. bis 13. Oktober 2019, ab 12 Uhr am Samstag bis 14 Uhr am Sonntag (wir freuen uns über jeden der im Anschluss noch ein wenig beim Putzen der Räume hilft)<br />
+                    <br />
+                    <b>Wo?</b> Jugendclub Hugo in Jena-Winzerla<br />
                     <br />
                     <b>Wie komme ich da hin?</b> https://www.jz-hugo.de/kontakt/<br />
                     <br />
-                    <b>Was kostest der Spaß?</b> {ticket} Euro (Geld das nach der Bezahlung der Geländekosten übrig bleibt, verteilen wir unter den Spielleitern)<br />
+                    <b>Was kostest der Spaß?</b> 10 Euro (Geld das nach der Bezahlung der Geländekosten übrig bleibt, verteilen wir gleichmäßig unter den Spielleitern)<br />
                     <br />
                     <b>Wie viele Teilnehmer?</b> Maximal 35<br />
                     <br />
-                    Für Essen und Trinken ist jeder selber verantwortlich. In unmittelbarer Nähe gibt es einen Rewe,
-                    ansonsten findet ihr sicherlich jemanden für eine gemeinsame Pizzabestellung. Wer möchte kann auch gerne
-                    auf dem Gelände übernachten – gebt uns in dem Fall bitte kurz Bescheid und bringt euch Feldbett, Schlafsack etc. mit.<br />
+                    Für Essen und Trinken ist jeder selber verantwortlich. In unmittelbarer Nähe gibt es einen Rewe und einen Aldi, ansonsten findet ihr sicherlich jemanden für eine gemeinsame Pizzabestellung. Wer möchte kann auch gerne auf dem Gelände übernachten – erwähnt das in diesem Fall einfach kurz in eurer Anmeldung und bringt euch Feldbett, Schlafsack etc. mit.<br />
                     <br />
-                    Wenn du kommen möchtest – worüber wir uns sehr freuen würden – dann erstelle dir zunächst
-                    einen Account auf dieser Website. Darauf hin erhältst du eine Mail mit Kontodaten. Sobald
-                    dein Geld eingetroffen ist, kannst du online eigene Spielrunden eintragen bzw. dich für angebotene
-                    Runden anmelden. Pro Zeitblock – jeweils ? Stunden, mit Ausnahme des Open-End-Nachtblocks – können
-                    maximal sieben Runden stattfinden.<br />
+                    Geplant sind von unserer Seite die folgenden Zeitblöcke:<br />
                     <br />
-                    Bei Fragen könnt ihr uns unter folgender E-Mail-Adresse kontaktieren:<br />
+                    Erster Block (Samstag Nachmittag) → 13-17 Uhr<br />
+
+                    Zweiter Block (Samstag Abend) → 18-22 Uhr<br />
+
+                    Dritter Block (Samstag Nacht) → ab 23 Uhr (Open End)<br />
+
+                    Vierter Block (Sonntag Vormittag) → 9-13 Uhr<br />
                     <br />
                     Freundliche Grüße,<br />
-                    Anne, Martin und Justus
+                    Anne, Daniel, Martin und Justus<br />
                 </p>
             </div>
         </div >
