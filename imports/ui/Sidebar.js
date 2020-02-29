b7793b243dec71f1989439aa5675486691603b91
otwald <hinterlanddev@googlemail.com>
2020-02-29 09:46:35 +0100

    added icons for smaller viewport

--- imports/ui/Sidebar.js
+++ imports/ui/Sidebar.js
@@ -18,7 +18,9 @@ const sidebar = props => {
     if (Meteor.userId()) {
         login = <li className="nav-item py-0">
             <div className={props.tab === 'account' ? `nav-link active` : "nav-link"}>
-                <button className='button' onClick={() => props.onTabChange('account')}>Account</button>
+                <button className='button' onClick={() => props.onTabChange('account')}>
+                    <i className='user_icon'></i><span className="d-none d-sm-block">Account</span>
+                </button>
             </div>
         </li>
         if (admin) {
@@ -35,31 +37,34 @@ const sidebar = props => {
             <li className="nav-item py-0">
                 <div className={props.tab === 'welcome' ? `nav-link active` : "nav-link"}>
                     <button className='button' onClick={() => props.onTabChange('welcome')} >
-                        <span className='home_icon'></span>
-                        Willkommen</button>
+                        <i className='home_icon'></i><span className="d-none d-sm-block">Willkommen</span>
+                    </button>
 
                 </div>
             </li>
             <li className="nav-item py-0">
                 <div className={props.tab === 'round' ? `nav-link active` : "nav-link"}>
-                    <button className='button' onClick={() => props.onTabChange('round')} >Spielerunde</button>
+                    <button className='button' onClick={() => props.onTabChange('round')} >
+                        <i className='box_icon'></i><span className="d-none d-sm-block">Spielerunde</span>
+                    </button>
                 </div>
             </li>
             {login}
             {adminInterface}
-            {/* <li className="nav-item py-0">
-                <AccountsUI />
-            </li> */}
             {Meteor.userId() != null ?
                 <li className="nav-item py-0">
                     <div className={props.tab === 'login' ? `nav-link active` : "nav-link"}>
-                        <button className='button' onClick={() => { Meteor.logout(); props.onTabChange('welcome') }} >Abmeldung</button>
+                        <button className='button' onClick={() => { Meteor.logout(); props.onTabChange('welcome') }} >
+                            <i className='key_icon'></i><span className="d-none d-sm-block">Abmeldung</span>
+                        </button>
                     </div>
                 </li>
                 :
                 <li className="nav-item py-0">
                     <div className={props.tab === 'login' ? `nav-link active` : "nav-link"}>
-                        <button className='button' onClick={() => props.onTabChange('login')} >Anmeldung</button>
+                        <button className='button' onClick={() => props.onTabChange('login')} >
+                            <i className='key_icon'></i><span className="d-none d-sm-block">Anmeldung</span>
+                        </button>
                     </div>
                 </li>
             }
